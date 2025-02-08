d2062040ee42b311dcb88f434d800b60cf052bab
angelali314159 <angelali314159@gmail.com>
2025-02-07 23:52:45 -0500

    Refactor InGameCustody component to improve state management and update button interactions

--- winghacks-25/src/app/in-game-custody/page.tsx
+++ winghacks-25/src/app/in-game-custody/page.tsx
@@ -3,48 +3,49 @@
 import { useState } from 'react';
 
 export default function InGameCustody() {
-  const [text, setText] = useState("The judge is talking");
-  const [screen, setScreen] = useState(0);
+  //const [judgeText, setText] = useState("The judge is talking");
+  const [screen, setScreen] = useState<string | null>(null);
 
-  const handleFirstButtonClick = () => {
-    setText("The judge says you're wrong");
-  };
 
-  const handleSecondButtonClick = () => {
-    setScreen(1);
-  };
-
-  const handleThirdButtonClick = () => {
-    setScreen(2);
-  };
-
-  if (screen === 1) {
+  if (screen === "proving-drug") {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+    
+  if (screen === "wrong") {
     return (
       <div>
         <h1>The judge is talking again</h1>
-        <button onClick={() => setText("Button 1 clicked")}>Button 1</button>
-        <button onClick={() => setText("Button 2 clicked")}>Button 2</button>
+        <button>Button 1</button>
+        <button>Button 2</button>
       </div>
     );
   }
 
-  if (screen === 2) {
+  if (screen === "living-with") {
     return (
       <div>
         <h1>The judge is talking again</h1>
-        <button onClick={() => setText("Button 1 clicked")}>Button 1</button>
-        <button onClick={() => setText("Button 2 clicked")}>Button 2</button>
-        <button onClick={() => setText("Button 3 clicked")}>Button 3</button>
+        <button>Button 1</button>
+        <button>Button 2</button>
       </div>
     );
   }
 
+  //defines the initial screen
   return (
     <div>
-      <h1>{text}</h1>
-      <button onClick={handleFirstButtonClick}>Button 1</button>
-      <button onClick={handleSecondButtonClick}>Button 2</button>
-      <button onClick={handleThirdButtonClick}>Button 3</button>
+      <h1>Hello. I hear you are seeking custody of your child. What are you arguing about?</h1>
+      <h2>Choose an option.</h2>
+      <p>I just hate who he is as a person! He is crazy and I hate his guts. That's why I divorced him.</p>
+      <button onClick={() => setScreen("wrong")}>Option 1</button>
+      <p>I believe he has a drug problem that stops him from making good judgments about our child.</p>
+      <button onClick={() => setScreen("proving-drug")}>Option 2</button>
+      <p>He just moved to a new house and the new lady he’s living with is insane! Sometimes I see scars on my son after he comes back and he’s always crying.</p>
+      <button onClick={() => setScreen("living-with")}>Option 3</button>
     </div>
   );
 }
