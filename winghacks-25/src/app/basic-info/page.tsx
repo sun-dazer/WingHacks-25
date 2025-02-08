36546aaa23a43ea90b1bf6fdef6dbdaab2188266
Claire Wang <wangclaire07@gmail.com>
2025-02-08 16:24:13 -0500

    Changed back arrow formatting

--- winghacks-25/src/app/basic-info/page.tsx
+++ winghacks-25/src/app/basic-info/page.tsx
@@ -19,13 +19,27 @@ export default function BasicInfo() {
     <div>
       <Image
         src="/images/back.png"
-        className="back-img"
+        className="inline back-img"
         alt="back"
         width={25}
         height={25}
         onClick={handleBackClick}
       />
-      <h1 className='h1'>Tutorial</h1>
+      <h1 className="inline h1">Tutorial</h1>
+      <div>
+        <div className='relative'>
+          <Image
+            src="/images/text box.png"
+            className="inline back-img"
+            alt="back"
+            width={650}
+            height={350}
+            onClick={handleBackClick}
+          />
+          <h2 className="absolute top-10 left-10 right-10 bottom-10">
+            This is an example sentence where we're gonna talk about law and lots of other things related to our game</h2>
+        </div>
+      </div>
     </div>
   );
 }
