0f0224b64c9e83a6c30bad535bb7248ad612186d
angelali314159 <angelali314159@gmail.com>
2025-02-07 23:56:03 -0500

    basic first page logic of custody in game

--- winghacks-25/src/app/in-game-custody/page.tsx
+++ winghacks-25/src/app/in-game-custody/page.tsx
@@ -3,10 +3,10 @@
 import { useState } from 'react';
 
 export default function InGameCustody() {
-  //const [judgeText, setText] = useState("The judge is talking");
+  //manages toggling between screens
   const [screen, setScreen] = useState<string | null>(null);
 
-
+  //************************ ALL THE SCREENS DEFINED ****************************** */
   if (screen === "proving-drug") {
     return (
       <div>
@@ -18,9 +18,8 @@ export default function InGameCustody() {
   if (screen === "wrong") {
     return (
       <div>
-        <h1>The judge is talking again</h1>
-        <button>Button 1</button>
-        <button>Button 2</button>
+        <h1>I'm sorry, I can't hear your case. You need an actual legal basis.</h1>
+        <button onClick={() => setScreen("initial")}>Try again</button>
       </div>
     );
   }
@@ -32,6 +31,21 @@ export default function InGameCustody() {
         <button>Button 1</button>
         <button>Button 2</button>
       </div>
+    );
+  }
+
+  if (screen === "initial") {
+    return (
+    <div>
+      <h1>Hello. I hear you are seeking custody of your child. What are you arguing about?</h1>
+      <h2>Choose an option.</h2>
+      <p>I just hate who he is as a person! He is crazy and I hate his guts. That's why I divorced him.</p>
+      <button onClick={() => setScreen("wrong")}>Option 1</button>
+      <p>I believe he has a drug problem that stops him from making good judgments about our child.</p>
+      <button onClick={() => setScreen("proving-drug")}>Option 2</button>
+      <p>He just moved to a new house and the new lady he’s living with is insane! Sometimes I see scars on my son after he comes back and he’s always crying.</p>
+      <button onClick={() => setScreen("living-with")}>Option 3</button>
+    </div>
     );
   }
 
