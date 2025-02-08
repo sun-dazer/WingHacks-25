56aa83dece4ebd537859681a5c6b2c57d909d950
angelali314159 <angelali314159@gmail.com>
2025-02-08 15:28:51 -0500

    Add fade-out animation and enhance in-game divorce UI layout

--- winghacks-25/src/app/in-game-divorce/page.tsx
+++ winghacks-25/src/app/in-game-divorce/page.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import Image from 'next/image';
 import { useUser } from '../UserContext';
+//import './styles.css';
 
 export default function InGameDivorce() {
   const router = useRouter();
@@ -12,8 +13,6 @@ export default function InGameDivorce() {
   const [screen, setScreen] = useState<string | null>(null);
 
   //************************ ALL THE SCREENS DEFINED ****************************** */
-  
-
   if (screen === "resided-hotel") {
     return (
       <div>
@@ -99,13 +98,29 @@ export default function InGameDivorce() {
 
   if (screen === "resided") {
     return (
-      <div>
-        <h1>Can you provide proof that you have resided in Florida for at least six months prior to filing for divorce?</h1>
-        <p>Yes, Your Honor. I have a hotel receipt showing I stayed in Miami for a week six months ago.</p>
-        <button onClick={() => setScreen("resided-hotel")}>Option 1</button>
-        <p>Yes, Your Honor. I have a lease agreement, utility bills in my name dating back over six months, and my Florida driver&apos;s license showing my current address.</p>
-        <button onClick={() => setScreen("continue-resided")}>Option 2</button>
+      <div className="flex justify-center mt-20">
+      <div className="flex flex-col">
+        <div className="flex-1 flex flex-row">
+          <div className="flex-1 mr-10">
+            <Image className="animate-float-right" src="/images/judge 2.png" alt="judge" width={500} height={500} />
+          </div>
+          <div className="flex-1 flex flex-col">
+            <div className="relative animate-options-float-in flex-1">
+              <Image className="absolute top-0 left-0 transform scale-x-125" src="/images/speech bubble skinny.png" alt="text box" width={661} height={500} />
+              <button className="absolute top-0 left-0 text-center text-black text-2xl mt-5" onClick={() => setScreen("resided")}>Yes, that is correct.</button>
+            </div>
+            <div className="relative animate-options-float-in flex-1">
+              <Image className="absolute top-0 left-0 transform scale-x-125" src="/images/speech bubble skinny.png" alt="text box" width={661} height={500} />
+              <button className="absolute top-0 left-0 text-center text-black text-2xl mt-5" onClick={() => setScreen("resided")}>Yes, that is correct.</button>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 relative">
+          <Image className="absolute top-0 left-0 transform scale-x-125 transform scale-y-80 animate-textbox-float-right " src="/images/text box.png" alt="text box" width={661} height={500} />
+          <h1 className="absolute top-10 left-0 w-full text-center text-black text-2xl animate-float-right">Hello, Ms. {name}. I hear you are seeking a divorce. </h1>
+        </div>
       </div>
+    </div>
     );
   }
 
@@ -170,13 +185,13 @@ export default function InGameDivorce() {
     );
   }
 
-  //defines the initial screen
+  //initial screen
   return (
     <div className="flex justify-center mt-20">
       <div className="flex flex-col">
         <div className="flex-1 flex flex-row">
           <div className="flex-1 mr-10">
-            <Image className="animate-float-right" src="/images/judge 1.png" alt="judge" width={500} height={500} />
+            <Image className="animate-float-right" src="/images/judge 2.png" alt="judge" width={500} height={500} />
           </div>
           <div className="flex-1">
             <div className="relative animate-options-float-in">
