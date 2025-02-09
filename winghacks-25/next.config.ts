26f46dcb3fabff4ed4c5d874545d97373200ed82
sun-dazer <147622828+sun-dazer@users.noreply.github.com>
2025-02-09 04:45:12 -0500

    fixed type

--- winghacks-25/next.config.ts
+++ winghacks-25/next.config.ts
@@ -1,16 +1,8 @@
 import type { NextConfig } from "next";
-import dotenv from 'dotenv';
-dotenv.config();
-
-module.exports = {
-  env: {
-    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
-  },
-};
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
+  output: 'export',
 };
 
 export default nextConfig;
